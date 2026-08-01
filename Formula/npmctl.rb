e1089480feaefb5465e260d9ec46571721f62af1
NightWatcher314 <43910105+NightWatcher314@users.noreply.github.com>
2026-08-01 08:42:45 +0000

    npmctl: update 0.5.3 bottle.

--- Formula/npmctl.rb
+++ Formula/npmctl.rb
@@ -6,10 +6,10 @@ class Npmctl < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/NightWatcher314/homebrew-formula/releases/download/npmctl-0.5.2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "881c770341b57bced39846b4b8b9b9ac304a0aae53786c4a7114a56e7121c301"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2a97d44f0bf1527f6b40f94fd1099fafe0817990f7ea0327fe1e179598b9bbee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7a0b49c529d5e628c18bdf1496010b797a04fa60724269c9ebea9d17d1d3ab7c"
+    root_url "https://github.com/NightWatcher314/homebrew-formula/releases/download/npmctl-0.5.3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "41c706a8433667c7883b0f864769250057a1d7463794dce52845bdedf5785314"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "6fa021f1d3e4760b94bda5205bb2ce7f3b8a127feadfe5b46b886627414214b6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "20360318b47751be362a515bbd0f62d446dcb72f28ec6bbc794d0f5ee54b7c1c"
   end
 
   depends_on "uv" => :build

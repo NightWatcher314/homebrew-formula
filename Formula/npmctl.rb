2f6f940a2e1a6d45fa954496e9741034bb7d38e8
NightWatcher <hitszzzf@gmail.com>
2026-08-01 16:37:04 +0800

    npmctl: update to 0.5.3

--- Formula/npmctl.rb
+++ Formula/npmctl.rb
@@ -1,13 +1,12 @@
 class Npmctl < Formula
   desc "CLI for Nginx Proxy Manager API automation"
   homepage "https://github.com/NightWatcher314/npmctl"
-  url "https://github.com/NightWatcher314/npmctl/archive/refs/tags/v0.5.2.tar.gz"
-  sha256 "27be09062e1c7135691acb3a7ddd68f255215d443cb2ed509096f4d287964ed8"
+  url "https://github.com/NightWatcher314/npmctl/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "68d8d577e55660e0beb985f18b0ef4b0bb98144c362008daf3c811da3fed32bc"
   license "MIT"
 
   bottle do
     root_url "https://github.com/NightWatcher314/homebrew-formula/releases/download/npmctl-0.5.2"
-    rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_tahoe:  "881c770341b57bced39846b4b8b9b9ac304a0aae53786c4a7114a56e7121c301"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "2a97d44f0bf1527f6b40f94fd1099fafe0817990f7ea0327fe1e179598b9bbee"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "7a0b49c529d5e628c18bdf1496010b797a04fa60724269c9ebea9d17d1d3ab7c"
